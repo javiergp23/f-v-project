47c55c079d4d883155b25949af6c097c23fa1c3c
javiergp23 <javgon2306@gmail.com>
2024-11-07 21:46:17 -0300

    add fix app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
-<script setup>
-export default {
-  data (){
-    return{
-      productos:[],
-    }
-  },
-  async function name(params) {
-    
-  }
-},
-
-</script>
-
 <template>
   <div>
-    <h1>Mis productos</h1>
-    <ul>
-      <li  v-for="producto in productos" :key="producto.id">
-        {{producto}}
-      </li>
-    </ul>
+      <h1>Mis Productos</h1>
+      <ul>
+          <li v-for="producto in productos" :key="producto.id">
+              {{ producto.nombre }} 
+          </li>
+      </ul>
   </div>
 </template>
 
-<style>
-
-</style>
+<script>
+import firebaseApp from '../../firebaseConfig.js'
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+export default {
+  name: 'FirebaseComponent',
+  data() {
+      return {
+          productos: [],
+      }
+  },
+  async mounted(){
+      try {
+          const db = getFirestore(firebaseApp)
+          const querySnapshot = await getDocs(collection(db,'productos'))
+          querySnapshot.forEach((doc) => {
+               this.productos.push({id:doc.id, ...doc.data()}) 
+               //this.productos.push({id:doc.id, nombre, tamaño,}) 
+          })
+      } catch (err) {
+          console.error(err)
+      }
+  }
+}
+</script>
