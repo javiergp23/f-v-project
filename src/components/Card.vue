e683aa0efc64cccabd978816b0cbe3597741f33e
javiergp23 <javgon2306@gmail.com>
2024-11-10 10:14:03 -0300

    Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,3 +4,13 @@
         
     }
 </script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{producto.nombre}}</h5>
+            <p class="card-text">{{producto.descripcion}}</p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+    </div>
+</template>
