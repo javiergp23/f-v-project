ce6148364e2ea0545c0be2d91c88491b7432d95f
javiergp23 <javgon2306@gmail.com>
2024-11-10 10:14:45 -0300

    style card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -14,3 +14,18 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+    .card {
+        max-width: 20rem;
+    }       
+    .card-title {
+        font-size: 1.5rem;
+    }
+    .card-text {
+        font-size: 1.2rem;
+    }
+    .card-body {
+        padding: 1rem;
+    }
+</style>
